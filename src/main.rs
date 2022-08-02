a54c5e5bc5ba8d732f160c3fe25c567683b9af23
Dwog <57990308+Dwogo@users.noreply.github.com>
2022-08-03 11:42:29 +1200

    The program now get's the user name

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use std::io::stdin;
+
 fn get_int() -> i64 {
     loop {
         let mut line = String::new();
-        std::io::stdin()
-            .read_line(&mut line)
-            .expect("Failed to read line");
+        stdin().read_line(&mut line).expect("Failed to read line");
 
         let line = match line.trim().parse() {
             Ok(num) => num,
@@ -17,8 +17,16 @@ fn get_int() -> i64 {
 }
 
 fn main() {
-    println!("Hello, world!");
-    println!("CBHS");
+    println!("Hello, and welcome to the Apple Inc. questionare");
+    println!("    ");
+    println!("Please input your name");
+
+    let mut usr_name = String::new();
+    stdin()
+        .read_line(&mut usr_name)
+        .expect("Failed to read user name");
+    let usr_name: String = usr_name.trim().parse().unwrap();
+    println!("Welcome, {usr_name}");
 
     let num = get_int();
     println!("{num}");
