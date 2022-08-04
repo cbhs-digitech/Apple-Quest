dd52b328eeb68da15f828c5a0867c7004264e370
Dwog <57990308+Dwogo@users.noreply.github.com>
2022-08-04 21:34:00 +1200

    Starting Q2 and finally after some struggle made the user see the 'rules'

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use std::io::stdin;
 
+fn get_string() -> String {
+    // Varable to store input
+    let mut line = String::new();
+    // Reading and storing user input
+    stdin().read_line(&mut line).expect("Failed to read line");
+    // This function is to trim out the unwanted whitespace
+    let line: String = line.trim().parse().unwrap();
+    return line;
+}
+
 // Getting an integer input from the user
-fn get_int() -> i64 {
+fn get_answer() -> i32 {
     loop {
         // The variable used to get the user input
         let mut line = String::new();
@@ -11,16 +21,47 @@ fn get_int() -> i64 {
         // Seeing if the user input is an interger or not
         // This method of redeclaring a variable is called shadowing so you dont have a headache with names
         let line = match line.trim().parse() {
-            // The input goes smooth sailing if it is a number
             Ok(num) => num,
-            // If the input is not a number then we tell the user to try again
             Err(_) => {
-                println!("Sorry that's not a number");
+                println!("Sorry that's not a valid answer (Please use a number)");
                 // The continue is here to tell the loop to skip all the other commands and do it again
                 continue;
             }
         };
+
+        if !(line > 0) || !(line < 5) {
+            println!("Sorry that's not a valid answer");
+            continue;
+        }
+
         return line;
+    }
+}
+
+fn rules() {
+    println!("The rules are that you answer in the number corrosponding to the answer");
+}
+
+fn quest_one() -> u32 {
+    // Question
+    println!("When did apple first reveal the iPhone?");
+
+    // Selection of answers
+    println!("1. 1999");
+    println!("2. 2001");
+    println!("3. 2005");
+    println!("4. 2007");
+
+    // Getting user's answer
+    let answer = get_answer();
+
+    // Seeing if user's answer is correct
+    if answer == 4 {
+        println!("Correct");
+        return 1;
+    } else {
+        println!("Incorrect");
+        return 0;
     }
 }
 
@@ -32,18 +73,21 @@ fn main() {
 
     // User name
     println!("Please input your name");
-
     // User input for name
-    let mut usr_name = String::new();
-    stdin()
-        .read_line(&mut usr_name)
-        .expect("Failed to read user name");
-    let usr_name: String = usr_name.trim().parse().unwrap();
-
+    let usr_name = get_string();
     // Printing user's name
     println!("Welcome, {usr_name}");
 
-    // Testing if the integer input function and printing it out
-    let num = get_int();
-    println!("{num}");
+    println!("    ");
+
+    // Asking if the user wants to see the currently barely written rules
+    println!("Do you want to here the rules (Yes/No)");
+    let yes_or_no: String = get_string().to_ascii_lowercase();
+    if yes_or_no == "y".to_string() || yes_or_no == "yes".to_string() {
+        rules();
+    }
+
+    println!("    ");
+    println!("Ok");
+    quest_one();
 }
